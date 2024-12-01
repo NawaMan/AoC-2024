23cc62b5e78b1bed0c3c93806c941b4b7f06646a
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-01 16:03:30 -0500

    Day 2 preparation.

---
+++ src/test/java/day2/Day2Part2Test.java
@@ -0,0 +1,38 @@
+package day2;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import common.AocCommon;
+import common.Testable;
+import functionalj.list.FuncList;
+
+@Ignore
+public class Day2Part2Test implements AocCommon, Testable {
+    
+    
+    Object calulate(FuncList<String> lines) {
+        return null;
+    }
+    
+    //== Test ==
+    
+    @Test
+    public void testExample() {
+        var lines = readAllLines();
+        System.out.println(lines);
+        
+        var result = calulate(lines);
+        assertAsString("", result);
+    }
+    
+    @Test
+    public void testProd() {
+        var lines = readAllLines();
+        System.out.println(lines);
+        
+        var result = calulate(lines);
+        assertAsString("", result);
+    }
+    
+}
