9d79c64f9d702549db3b8d47cbb47025efbd7952
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-04 07:57:45 -0500

    WIP

--- src/test/java/day4/Day4Part1Test.java
+++ src/test/java/day4/Day4Part1Test.java
@@ -1,120 +1,77 @@
 package day4;
 
+import static functionalj.stream.intstream.IntStreamPlus.range;
+
+import java.util.function.IntBinaryOperator;
+
 import org.junit.Test;
 
 import common.BaseTest;
 import functionalj.list.FuncList;
+import functionalj.list.intlist.IntFuncList;
 
 public class Day4Part1Test extends BaseTest {
     
-    char[] expected = "XMAS".toCharArray();
+    record Grid(FuncList<String> lines) {
+        char charAt(int r, int c) {
+            if ((r < 0) || (r >= lines.size()))          return '.';
+            if ((c < 0) || (c >= lines.get(r).length())) return '.';
+            return lines.get(r).charAt(c);
+        }
+        IntFuncList loop(IntBinaryOperator operator) {
+            var rows = lines.size();
+            var cols = lines.get(0).length();
+            return range(0, rows).flatMapToInt(r -> {
+                        return range(0, cols).mapToInt(c -> {
+                            return operator.applyAsInt(r, c);
+                        });
+                    })
+                    .toFuncList();
+        }
+    }
+    record WordGrid(Grid grid) {
+        
+        int wordCount(String word) {
+            var first = word.charAt(0);
+            return grid.loop((r, c) -> {
+                return (grid.charAt(r, c) == first)
+                        ? findWords(word, r, c)
+                        : 0;
+            }).sum();
+        }
+        
+        private int findWords(String word, int r, int c) {
+            int total = 0;
+            total += findWord(r, c,  0,  1, word) ? 1 : 0;
+            total += findWord(r, c,  0,  0, word) ? 1 : 0;
+            total += findWord(r, c,  0, -1, word) ? 1 : 0;
+            total += findWord(r, c,  1,  1, word) ? 1 : 0;
+            total += findWord(r, c,  1,  0, word) ? 1 : 0;
+            total += findWord(r, c,  1, -1, word) ? 1 : 0;
+            total += findWord(r, c, -1,  1, word) ? 1 : 0;
+            total += findWord(r, c, -1,  0, word) ? 1 : 0;
+            total += findWord(r, c, -1, -1, word) ? 1 : 0;
+            return total;
+        }
+        private boolean findWord(int r, int c, int directionR, int directionC, String word) {
+            return findWord(r, c, directionR, directionC, 1, word);
+        }
+        private boolean findWord(int r, int c, int directionR, int directionC, int at, String word) {
+            if (at >= word.length())
+                return true;
+            
+            r += directionR;
+            c += directionC;
+            return grid.charAt(r, c) == word.charAt(at)
+                && findWord(r, c, directionR, directionC, at + 1, word);
+        }
+    }
     
     Object calulate(FuncList<String> lines) {
-        int total = 0;
-        
-        int size   = lines.size();
-        int length = lines.get(0).length();
-        for (int r = 0; r < size; r++) {
-            for (int c = 0; c < length; c++) {
-                var ch = charAt(r, c, lines);
-                if (ch == 'X') {
-                    total += findWord(r, c, lines, 1, expected,  0,  1) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected,  0,  0) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected,  0, -1) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected,  1,  1) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected,  1,  0) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected,  1, -1) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected, -1,  1) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected, -1,  0) ? 1 : 0;
-                    total += findWord(r, c, lines, 1, expected, -1, -1) ? 1 : 0;
-                }
-            }
-        }
-        return total;
+        var wordGrid  = new WordGrid(new Grid(lines));
+        return wordGrid.wordCount("XMAS");
     }
     
-    boolean findWord(int r, int c, FuncList<String> lines, int at, char[] expected, int directionR, int directionC) {
-        if (at >= expected.length)
-            return true;
-        
-        r += directionR;
-        c += directionC;
-        
-        var ch = charAt(r, c, lines);
-        if (ch != expected[at])
-            return false;
-        
-        return findWord(r, c, lines, at + 1, expected, directionR, directionC);
-    }
-    
-    
-    char charAt(int r, int c, FuncList<String> lines) {
-        if ((r < 0) || (r >= lines.size()))
-            return '.';
-        if ((c < 0) || (c >= lines.get(r).length()))
-            return '.';
-        return lines.get(r).charAt(c);
-    }
-//    
-//    Object calulate(FuncList<String> lines) {
-//        var line0   = lines;
-//        var line45  = rotate45(line0);
-//        var line90  = rotate90(lines);
-//        var line135 = rotate45(line90);
-//        var line180 = rotate90(line90);
-//        var line225 = rotate45(line180);
-//        var line270 = rotate90(line180);
-//        var line315 = rotate45(line270);
-//        
-//        return FuncList.of(line0, line45, line90, line135, line180, line225, line270, line315)
-//                .mapToInt(this::countWord).sum();
-//    }
-//    
-//    int countWord(FuncList<String> lines) {
-//        return countWord(lines, "XMAS");
-//    }
-//    
-//    int countWord(FuncList<String> lines, String word) {
-//        return lines.mapToInt(line -> StrFuncs.grab(line, word).size()).sum();
-//    }
-//    
-//    FuncList<String> rotate90(FuncList<String> lines) {
-//        var size = lines.size();
-//        return range(0, lines.get(0).length())
-//                .mapToObj(r -> range(0, size).mapToObj(c -> lines.get(c).charAt(r)).join())
-//                .toFuncList();
-//    }
-//    
-//    char charAt(int r, int c, FuncList<String> lines) {
-//        if ((r < 0) || (r >= lines.size()))
-//            return '.';
-//        if ((c < 0) || (c >= lines.get(r).length()))
-//            return '.';
-//        return lines.get(r).charAt(c);
-//    }
-//    
-//    FuncList<String> rotate45(FuncList<String> lines) {
-//        int size   = lines.size();
-//        int length = lines.get(0).length();
-//        int width   = size + length;
-//        var grid = new int[width + 1][width + 1];
-//        
-//        for (int y = 0; y < grid.length; y++) {
-//            for (int x = 0; x < grid[y].length; x++) {
-//                grid[y][x] = '.';
-//            }
-//        }
-//        
-//        for (int r = 0; r < size; r++) {
-//            for (int c = 0; c < length; c++) {
-//                int x = +c + -r + size;
-//                int y = -c + -r + size + length;
-//                grid[y][x] = charAt(r, c, lines);
-//            }
-//        }
-//        return FuncList.of(grid).map(row -> IntFuncList.of(row).mapToObj(ch -> (char)ch).join().replaceAll("(.)\\.", "$1"));
-//    }
-//    
     //== Test ==
     
     @Test
--- src/test/java/day4/Day4Part2Test.java
+++ src/test/java/day4/Day4Part2Test.java
@@ -1,42 +1,61 @@
 package day4;
 
+import static functionalj.stream.intstream.IntStreamPlus.range;
+
+import java.util.function.IntBinaryOperator;
+
 import org.junit.Test;
 
 import common.BaseTest;
 import functionalj.list.FuncList;
+import functionalj.list.intlist.IntFuncList;
 
 public class Day4Part2Test extends BaseTest {
     
+    record Grid(FuncList<String> lines) {
+        char charAt(int r, int c) {
+            if ((r < 0) || (r >= lines.size()))          return '.';
+            if ((c < 0) || (c >= lines.get(r).length())) return '.';
+            return lines.get(r).charAt(c);
+        }
+        IntFuncList loop(IntBinaryOperator operator) {
+            var rows = lines.size();
+            var cols = lines.get(0).length();
+            return range(0, rows).flatMapToInt(r -> {
+                        return range(0, cols).mapToInt(c -> {
+                            return operator.applyAsInt(r, c);
+                        });
+                    })
+                    .toFuncList();
+        }
+    }
+    record WordGrid(Grid grid) {
+        int wordX(String word) {
+            if (word.length() != 3)
+                throw new IllegalArgumentException("Only 3-character-long word is supported.");
+            
+            var mid    = word.charAt(1);
+            var before = word.charAt(0);
+            var after  = word.charAt(2);
+            return grid.loop((r, c) -> {
+                return (grid.charAt(r, c) != mid)
+                        ? 0
+                        : checkX(r, c, before, after) ? 1 : 0;
+            }).sum();
+        }
+        boolean checkX(int r, int c, char before, char after) {
+            if ((grid.charAt(r - 1, c - 1) == before && grid.charAt(r + 1, c + 1) == after)
+             || (grid.charAt(r - 1, c - 1) == after  && grid.charAt(r + 1, c + 1) == before)) {
+                return ((grid.charAt(r - 1, c + 1) == before && grid.charAt(r + 1, c - 1) == after)
+                     || (grid.charAt(r - 1, c + 1) == after  && grid.charAt(r + 1, c - 1) == before));
+            }
+            return false;
+        }
+    }
     
     Object calulate(FuncList<String> lines) {
-        int total = 0;
-        
-        int size = lines.size();
-        for (int r = 0; r < size; r++) {
-            int length = lines.get(r).length();
-            for (int c = 0; c < length; c++) {
-                var ch = charAt(r, c, lines);
-                if (ch == 'A') {
-                    total += findX(r, c, lines) ? 1 : 0;
-                }
-            }
-        }
-        return total;
-    }
-    
-    boolean findX(int r, int c, FuncList<String> lines) {
-        if ((charAt(r - 1, c - 1, lines) == 'M' && charAt(r + 1, c + 1, lines) == 'S')
-         || (charAt(r - 1, c - 1, lines) == 'S' && charAt(r + 1, c + 1, lines) == 'M')) {
-            return ((charAt(r - 1, c + 1, lines) == 'M' && charAt(r + 1, c - 1, lines) == 'S')
-                 || (charAt(r - 1, c + 1, lines) == 'S' && charAt(r + 1, c - 1, lines) == 'M'));
-        }
-        return false;
-    }
-    
-    char charAt(int r, int c, FuncList<String> lines) {
-        if ((r < 0) || (r >= lines.size()))          return '.';
-        if ((c < 0) || (c >= lines.get(r).length())) return '.';
-        return lines.get(r).charAt(c);
+        var wordGrid = new WordGrid(new Grid(lines));
+        return wordGrid.wordX("MAS");
     }
     
     //== Test ==
