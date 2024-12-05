6178e23b6b6d824e65b01622e16eff58874223ea
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-05 02:05:22 -0500

    Refactor -- Cleanup

--- src/test/java/day5/Day5Part1Test.java
+++ src/test/java/day5/Day5Part1Test.java
@@ -1,6 +1,7 @@
 package day5;
 
 import static functionalj.functions.StrFuncs.grab;
+import static java.lang.Integer.parseInt;
 
 import org.junit.Test;
 
@@ -9,44 +10,33 @@ import functionalj.list.FuncList;
 
 public class Day5Part1Test extends BaseTest {
     
-    
     Object calulate(FuncList<String> lines) {
-        var firstPart = lines.acceptUntil(""::equals);
-        var lastPart  = lines.skip(firstPart.count() + 1);
-        
-        var rules = firstPart.map(grab("[0-9]+"));
-        
-        rules
-        .forEach(this::println);
-        println();
-        
-        var updates = lastPart.map(grab("[0-9]+"));
-        
-        updates
-        .forEach(this::println);
-        println();
-        
+        var firstSection = lines.acceptUntil(""::equals);
+        var lastSection  = lines.skip       (firstSection.count() + 1);
+        var rules        = firstSection.map(grab("[0-9]+"));
+        var updates      = lastSection .map(grab("[0-9]+"));
         return updates
-        .filter(update -> {
-            return rules.allMatch(rule -> {
-                var clone = update.toMutableList();
-                clone.retainAll(rule);
-                if (clone.size() != 2) {
-                    return true;
-                }
-                
-                return clone.toString().equals(rule.toString());
-            });
-        })
-        .mapToInt(update -> Integer.parseInt(update.get(update.size() / 2)))
-        .sum();
+                .filter  (update -> correctOrderUpdate(rules, update))
+                .map     (update -> update.get(update.size() / 2))
+                .mapToInt(middle -> parseInt(middle))
+                .sum();
+    }
+    
+    private boolean correctOrderUpdate(FuncList<FuncList<String>> rules, FuncList<String> update) {
+        return rules.allMatch(rule -> {
+            // update=75,47,61,53,29  intersect  rule=47|29  =>  matchOrder=[47,29]   <---  correct
+            // update=75,47,61,53,29  intersect  rule=61|13  =>  matchOrder=[61]      <---  incorrect
+            var matchOrder = update.filterIn(rule);
+            return (matchOrder.size() != 2)
+                    || matchOrder.equals(rule);
+        });
     }
     
     //== Test ==
     
     @Test
     public void testExample() {
-        var lines  = readAllLines();
+        var lines  = readAllLines().toCache();
         var result = calulate(lines);
         println("result: " + result);
         assertAsString("143", result);
@@ -54,7 +44,7 @@ public class Day5Part1Test extends BaseTest {
     
     @Test
     public void testProd() {
-        var lines  = readAllLines();
+        var lines  = readAllLines().toCache();
         var result = calulate(lines);
         println("result: " + result);
         assertAsString("5747", result);
--- src/test/java/day5/Day5Part2Test.java
+++ src/test/java/day5/Day5Part2Test.java
@@ -1,5 +1,6 @@
 package day5;
 
+import static functionalj.function.Func.itself;
 import static functionalj.functions.StrFuncs.grab;
 import static java.lang.Integer.parseInt;
 
@@ -11,56 +12,75 @@ import functionalj.list.FuncList;
 public class Day5Part2Test extends BaseTest {
     
     Object calulate(FuncList<String> lines) {
-        var firstPart = lines.acceptUntil(""::equals);
-        var lastPart  = lines.skip(firstPart.count() + 1);
-        
-        var rules = firstPart.map(grab("[0-9]+"));
-        var updates = lastPart.map(grab("[0-9]+"));
-        
-        var incorrects = updates
-        .filter(update -> {
-            return !rules.allMatch(rule -> {
-                var clone = update.toMutableList();
-                clone.retainAll(rule);
-                if (clone.size() != 2) {
-                    return true;
-                }
-                
-                return clone.toString().equals(rule.toString());
-            });
-        });
-        
-        return incorrects.mapToInt(update -> {
-            var updateRules = rules.filter(rule -> {
-                var clone = update.toMutableList();
-                clone.retainAll(rule);
-                return clone.size() == 2;
-            });
-            
-            var middle = reorder(updateRules);
-            return parseInt(middle);
-        }).sum();
+        var firstSection = lines.acceptUntil(""::equals);
+        var lastSection  = lines.skip       (firstSection.count() + 1);
+        var rules        = firstSection.map (grab("[0-9]+"));
+        var updates      = lastSection .map (grab("[0-9]+"));
+        return updates
+                .filter  (update   -> incorrectUpdate(rules, update))
+                .map     (update   -> relevantRules  (rules, update))
+                .map     (relRules -> findMiddlePage (relRules))
+                .mapToInt(middle   -> parseInt(middle))
+                .sum();
     }
     
-    private String reorder(FuncList<FuncList<String>> updateRules) {
-        var lastPages  = updateRules.map(rule -> rule.get(1));
-        var firstPage  = updateRules.map(rule -> rule.get(0)).exclude(lastPages::contains).findFirst().get();
-        var firstPages = updateRules.map(rule -> rule.get(0));
-        var lastPage   = updateRules.map(rule -> rule.get(1)).exclude(firstPages::contains).findFirst().get();
+    boolean incorrectUpdate(FuncList<FuncList<String>> rules, FuncList<String> update) {
+        return rules.anyMatch(rule -> {
+            // update=75,47,61,53,29  intersect  rule=47|29  =>  matchOrder=[47,29]   <---  correct
+            // update=75,47,61,53,29  intersect  rule=61|13  =>  matchOrder=[61]      <---  incorrect
+            var matchOrder = update.filterIn(rule);
+            return (matchOrder.size() == 2) 
+                    && !matchOrder.toString().equals(rule.toString());
+        });
+    }
+    
+    FuncList<FuncList<String>> relevantRules(FuncList<FuncList<String>> rules, FuncList<String> update) {
+        return rules.filter(rule -> {
+            // update=75,47,61,53,29  intersect  rule=47|29  =>  matchOrder=[47,29]   <---  relevant
+            // update=75,47,61,53,29  intersect  rule=61|13  =>  matchOrder=[61]      <---  irrelevant
+            var matchOrder = update.filterIn(rule);
+            return matchOrder.size() == 2;
+        });
+    }
+    
+    String findMiddlePage(FuncList<FuncList<String>> rules) {
+        // All pages     : 61,13,29
+        // Relevant rules: 61|13  29|13  61|29
         
-        var shrinkPages = updateRules.flatMap(rule -> rule).excludeAny(firstPage, lastPage).distinct();
-        if (shrinkPages.size() == 1)
-            return shrinkPages.get(0);
+        // First pages: 61, 29, 61  ->  61,29
+        // Last pages : 13, 13, 29  ->  13,29
+        var firstPages = rules.map(rule -> rule.get(0));
+        var lastPages  = rules.map(rule -> rule.get(1));
         
-        var shrinkUpdates = updateRules.exclude(rule -> rule.get(0).equals(firstPage) || rule.last().get().equals(lastPage));
-        return reorder(shrinkUpdates);
+        // First page: [61,29] - [13,29]  ->  61
+        // Last page : [13,29] - [61,29]  ->  13
+        var firstPage = firstPages.exclude(lastPages::contains) .findFirst().get();
+        var lastPage  = lastPages .exclude(firstPages::contains).findFirst().get();
+        
+        // Middle Pages = [61,13,29] - [61] - [13]  ->  29
+        var middlePages
+                = rules
+                .flatMap(itself())
+                .exclude(firstPage)
+                .exclude(lastPage)
+                .distinct();
+        if (middlePages.size() == 1)
+            return middlePages.get(0);
+        
+        var reduceRules
+                = rules
+                .exclude(rule -> rule.get(0).equals(firstPage))
+                .exclude(rule -> rule.last().get().equals(lastPage));
+        
+        // Recursively reduce.
+        return findMiddlePage(reduceRules);
     }
     
     //== Test ==
     
     @Test
     public void testExample() {
-        var lines  = readAllLines();
+        var lines  = readAllLines().toCache();
         var result = calulate(lines);
         println("result: " + result);
         assertAsString("123", result);
@@ -68,7 +88,7 @@ public class Day5Part2Test extends BaseTest {
     
     @Test
     public void testProd() {
-        var lines  = readAllLines();
+        var lines  = readAllLines().toCache();
         var result = calulate(lines);
         println("result: " + result);
         assertAsString("5502", result);
