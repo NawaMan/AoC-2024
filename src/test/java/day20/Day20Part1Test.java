227eee77c539b890d1341e8d9fe92070e593f31c
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-20 03:28:28 -0500

    Day 20

--- src/test/java/day20/Day20Part1Test.java
+++ src/test/java/day20/Day20Part1Test.java
@@ -5,14 +5,10 @@ import static java.lang.Long.MAX_VALUE;
 import static java.lang.Math.abs;
 import static java.util.Comparator.comparing;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.IntPredicate;
 
 import org.junit.Test;
 
@@ -20,18 +16,10 @@ import common.BaseTest;
 import functionalj.list.FuncList;
 import functionalj.list.FuncListBuilder;
 import functionalj.map.FuncMap;
-import functionalj.tuple.Tuple2;
 
 public class Day20Part1Test extends BaseTest {
-
-    public static final String BOLD   = "\033[1m";
-    public static final String RED    = "\033[31m";
-    public static final String GREEN  = "\033[32m";
-    public static final String YELLOW = "\033[33m";
-    public static final String BLUE   = "\033[34m";
-    public static final String RESET  = "\033[0m"; // Reset to default color
     
-    record Position(int row, int col) implements Comparable<Position> {
+    record Position(int row, int col) {
         FuncList<Position> neighbours() {
             return FuncList.of(
                         new Position(row + 1, col()    ),
@@ -39,18 +27,11 @@ public class Day20Part1Test extends BaseTest {
                         new Position(row - 1, col()    ),
                         new Position(row    , col() - 1));
         }
-        @Override
-        public String toString() {
-            return "(%d, %d)".formatted(row, col);
-        }
-        @Override
-        public int compareTo(Position o) {
-            return comparing(Position::row)
-                    .thenComparing(Position::col)
-                    .compare(this, o);
-        }
     }
-
+    
+    record Edge(Position start, Position end, int distance) {}
+    record Shortest(long distance, FuncList<Position> path) {}
+    
     record Grid(String[][] data, Position start, Position end) {
         
         static Grid from(FuncList<String> lines) {
@@ -68,90 +49,25 @@ public class Day20Part1Test extends BaseTest {
                     if (ch == 'E') { end   = new Position(r, c); data[r][c] = "."; }
                 }
             }
-            
             return new Grid(data, start, end);
         }
         
-        int width() {
-            return data.length;
-        }
-        
-        int height() {
-            return data[0].length;
-        }
-        
-        String at(Position position) {
-            return at(position.row, position.col);
-        }
-        String at(int row, int col) {
-            if (row < 0 || row >= data.length)      return "#";
-            if (col < 0 || col >= data[row].length) return "#";
-            return data[row][col];
-        }
         char charAt(Position position) {
-            return at(position).charAt(0);
-        }
-        char charAt(int row, int col) {
-            return at(row, col).charAt(0);
+            return data[position.row][position.col].charAt(0);
         }
         FuncList<Position> positions() {
             return range(0, data.length).toCache().flatMapToObj(row -> {
                 return range(0, data[0].length)
-                        .toCache ()
                         .mapToObj(col -> new Position(row, col))
                         .filter  (pos -> '.' == charAt(pos))
                         ;
             });
         }
-        FuncList<Position> select(IntPredicate charSelector) {
-            return range(0, data.length).toCache().flatMapToObj(row -> {
-                return range(0, data[0].length)
-                        .toCache ()
-                        .filter  (col -> charSelector.test((int)charAt(row, col)))
-                        .mapToObj(col -> new Position(row, col))
-                        ;
-            });
-        }
-        
-        protected Grid clone() {
-            var clone = new String[data.length][];
-            for (int i = 0; i < clone.length; i++) {
-                clone[i] = data[i].clone();
-            }
-            return new Grid(clone, start, end);
-        }
-        
-        @Override
-        public String toString() {
-            return FuncList.of(data).map(chs -> FuncList.of(chs).join()).join("\n");
-        }
-    }
-    
-    record Edge(Position start, Position end, int distance) {
-        Position to(Position from) {
-            return from.equals(start) ? end   :
-                   from.equals(end)   ? start : null; 
-        }
-        @Override
-        public String toString() {
-            return "Edge[(%d,%d)->(%d,%d) : %d]"
-                    .formatted(start.row, start.col, end.row, end.col, distance);
-        }
     }
     
     record Graph(Grid grid, Position start, Position end, FuncList<Position> nodes, FuncMap<Position, FuncList<Edge>> graphMap) {
-        record NodeInfo(Position current, long distance, Position previous) {
-            @Override
-            public String toString() {
-                return "(%d,%d) : %s (from (%s, %s))".formatted(
-                        current.row, 
-                        current.col, 
-                        distance, 
-                        (previous == null) ? "null" : previous.row, 
-                        (previous == null) ? "null" : previous.col);
-            }
-        }
-
+        record NodeInfo(Position current, long distance, Position previous) {}
+        
         static Graph from(Grid grid) {
             var graphMap = new ConcurrentHashMap<Position, FuncListBuilder<Edge>>();
             var banches  = grid.positions().cache();
@@ -167,21 +83,13 @@ public class Day20Part1Test extends BaseTest {
             });
             
             var map = FuncMap.from(graphMap).mapValue(FuncListBuilder::build);
-//            map.entries().sortedBy(Map.Entry::getKey).forEach(println);
             return new Graph(grid, grid.start, grid.end, banches, map);
         }
         
-        Tuple2<Long, FuncList<Position>> shortestCostPath() {
-            return shortestCostPath(true);
-        }
-        
-        Tuple2<Long, FuncList<Position>> shortestCostPath(boolean showPath) {
+        Shortest shortestPath() {
             var visiteds  = new HashSet<Position>();
             var nodeInfos = new LinkedHashMap<Position, NodeInfo>();
             var nextInfos = new PriorityQueue<NodeInfo>(comparing(n -> n.distance));
-            
-//            System.out.println("nodes: ");
-//            nodes.stream().map(String::valueOf).map("  "::concat).forEach(println);
             
             var beforeStart = new Position(start.row, start.col - 1);
             nodes.forEach(node -> {
@@ -193,9 +101,6 @@ public class Day20Part1Test extends BaseTest {
                 nextInfos.add(nodeInfo);
             });
             
-//            System.out.println("nextInfos: ");
-//            nextInfos.stream().map(String::valueOf).map("  "::concat).forEach(println);
-            
             var current      = start;
             var currDistance = 0L;
             
@@ -206,123 +111,58 @@ public class Day20Part1Test extends BaseTest {
                 nextInfos.remove(currInfo);
                 visiteds.add(currNode);
                 
-//                System.out.println("Current: " + currNode);
                 var nextNodes = graphMap.get(currNode);
-                if (nextNodes != null) {
-                    nextNodes.forEach(next -> {
-                        var nextNode = next.to(currNode);
-                        if (visiteds.contains(nextNode))
-                            return;
-                        
-//                        System.out.println("Next: " + nextNode);
-                        if (currInfo.previous == null)
-                            System.out.print("");
-                        
-                        var nextInfo = nodeInfos.get(nextNode);
-                        var distance = next.distance;
-                        if (distance < nextInfo.distance) {
-                            nextInfo = new NodeInfo(nextNode, currDist + distance, currNode);
-                            nodeInfos.put(nextNode, nextInfo);
-                            nextInfos.remove(currInfo);
-                            nextInfos.add(nextInfo);
-                        }
-                    });
-                }
-    
-                var nextInfo = nextInfos.poll();
-                if (nextInfo == null)
-                    break;
+                nextNodes.forEach(next -> {
+                    var nextNode = next.end();
+                    if (visiteds.contains(nextNode))
+                        return;
+                    
+                    var nextInfo = nodeInfos.get(nextNode);
+                    var distance = next.distance;
+                    if (distance < nextInfo.distance) {
+                        nextInfo = new NodeInfo(nextNode, currDist + distance, currNode);
+                        nodeInfos.put(nextNode, nextInfo);
+                        nextInfos.remove(currInfo);
+                        nextInfos.add(nextInfo);
+                    }
+                });
                 
+                var nextInfo = nextInfos.poll();
                 current      = nextInfo.current;
                 currDistance = nextInfo.distance;
             }
             
-//            System.out.println("Node: ");
-//            nodeInfos.entrySet().forEach(println);
-            
-            var path    = new FuncListBuilder<Position>();
-            var display = grid.clone();
+            var path = new FuncListBuilder<Position>();
             var node = end;
-            var i    = 0;
             while (node != start) {
                 path.add(node);
-                display.data[node.row][node.col] = BOLD + BLUE + "#" + RESET;
-                
-                if (showPath)
-                    System.out.print("  (%2d, %2d)".formatted((node == null) ? -1 : node.row, (node == null) ? -1 : node.col));
-                i++;
-                if (i == 10) {
-                    i = 0;
-                    if (showPath)
-                        System.out.println();
-                }
-                
-                var nodeInfo = nodeInfos.get(node);
-                if (nodeInfo == null)
-                    break;
-                
-                node = nodeInfo.previous;
+                node = nodeInfos.get(node).previous;
             }
-            if (node == start) {
-                path.add(start);
-            }
+            path.add(start);
             
-            display.data[end.row][end.col]     = BOLD + GREEN + "#" + RESET;
-            display.data[start.row][start.col] = BOLD + RED + "#" + RESET;
-            
-            if (showPath) {
-                System.out.println();
-                System.out.println(display);
-            }
-            
-            var endNodeInfo = nodeInfos.get(end);
-            if (endNodeInfo == null) {
-                return Tuple2.of(Long.MAX_VALUE, FuncList.empty());
-            }
-            
-            var shortestDistance = endNodeInfo.distance;
-            return Tuple2.of(shortestDistance, path.build().reverse());
+            var shortestDistance = nodeInfos.get(end).distance;
+            return new Shortest(shortestDistance, path.build().reverse().cache());
         }
     }
     
-    Object calulate(FuncList<String> lines) {
-        var grid = Grid.from(lines);
-        
-        println(grid);
-        
+    Object calulate(FuncList<String> lines, int allowCheat, int atLeastSaved) {
+        var grid  = Grid.from(lines);
         var graph = Graph.from(grid);
-//        graph.graphMap.entries().sortedBy(Map.Entry::getKey).forEach(println);;
-        var shortestPath = graph.shortestCostPath();
-        println(shortestPath);
         
-        var path          = shortestPath._2();
-        var nodeWithIndex = path.mapWithIndex();
-        
-        println(nodeWithIndex);
-        
-        var savedCounts = new HashMap<Integer, AtomicInteger>();
-        var count = 0;
-        for (var one : nodeWithIndex) {
-            for (var two : nodeWithIndex) {
+        var shortestPath = graph.shortestPath();
+        var nodeWithIndex = shortestPath.path.mapWithIndex();
+        return nodeWithIndex.sumToInt(one -> {
+            return nodeWithIndex.filter(two -> {
                 var oneOrder = one._1;
                 var twoOrder = two._1;
-                if ((twoOrder - oneOrder) < 4)
-                    continue;
-                
-                var onePos = one._2;
-                var twoPos = two._2;
+                var onePos   = one._2;
+                var twoPos   = two._2;
                 var cheatTime = abs(onePos.row() - twoPos.row()) + abs(onePos.col() - twoPos.col());
                 var savedTime = twoOrder - oneOrder - cheatTime;
-                
-                if (cheatTime <= 2 && savedTime >= 100)
-                    count++;
-            }
-        }
-        
-//        FuncMap.from(savedCounts).entries().filter(entry -> entry.getKey() >= 100).sortedBy(Map.Entry::getKey).forEach(println);
-        
-//        return FuncMap.from(savedCounts).entries().filter(entry -> entry.getKey() >= 100).map(Map.Entry::getValue).sumToInt(AtomicInteger::intValue);
-        return count;
+                return (cheatTime <= allowCheat && savedTime >= atLeastSaved);
+            })
+            .size();
+        });
     }
     
     //== Test ==
@@ -330,7 +170,7 @@ public class Day20Part1Test extends BaseTest {
     @Test
     public void testExample() {
         var lines  = readAllLines();
-        var result = calulate(lines);
+        var result = calulate(lines, 2, 100);
         println("result: " + result);
         assertAsString("0", result);
     }
@@ -338,7 +178,7 @@ public class Day20Part1Test extends BaseTest {
     @Test
     public void testProd() {
         var lines  = readAllLines();
-        var result = calulate(lines);
+        var result = calulate(lines, 2, 100);
         println("result: " + result);
         assertAsString("1317", result);
     }
--- src/test/java/day20/Day20Part2Test.java
+++ src/test/java/day20/Day20Part2Test.java
@@ -1,158 +1,17 @@
 package day20;
 
-import static day20.Day20Part1Test.BLUE;
-import static day20.Day20Part1Test.BOLD;
-import static day20.Day20Part1Test.GREEN;
-import static day20.Day20Part1Test.RED;
-import static day20.Day20Part1Test.RESET;
-import static day20.Day20Part1Test.YELLOW;
-import static java.lang.Math.abs;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 import common.BaseTest;
-import day20.Day20Part1Test.Graph;
-import day20.Day20Part1Test.Grid;
-import day20.Day20Part1Test.Position;
-import functionalj.list.FuncList;
-import functionalj.map.FuncMap;
-import functionalj.tuple.Tuple2;
 
 public class Day20Part2Test extends BaseTest {
-    
-    Object calulate(FuncList<String> lines) {
-        var grid = Grid.from(lines);
-        
-        println(grid);
-        
-        var graph = Graph.from(grid);
-//        graph.graphMap.entries().sortedBy(Map.Entry::getKey).forEach(println);;
-        var shortestPath = graph.shortestCostPath(true);
-        var shortestDist = shortestPath._1();
-//        println(shortestPath);
-        
-        var path          = shortestPath._2();
-        var nodeWithIndex = path.mapWithIndex();
-        
-//        println(nodeWithIndex);
-        
-        var invertGrid  = invertGrid(grid);
-        var seenCuts    = new HashMap<Long, Set<String>>();
-        var savedCounts = new HashMap<Long, AtomicInteger>();
-        
-        var count = 0;
-        for (var one : nodeWithIndex) {
-            for (var two : nodeWithIndex) {
-                var oneOrder = one._1;
-                var twoOrder = two._1;
-                if ((twoOrder - oneOrder) < 4)
-                    continue;
-                
-                var onePos = one._2;
-                var twoPos = two._2;
-                var cheatTime = abs(onePos.row() - twoPos.row()) + abs(onePos.col() - twoPos.col());
-                var savedTime = twoOrder - oneOrder - cheatTime;
-                
-                if (cheatTime <= 20 && savedTime >= 100)
-                    count++;
-
-////                
-////                if (((onePos.row() == 3) && (onePos.col() == 1))
-////                 && ((twoPos.row() == 7) && (twoPos.col() == 3))) {
-//                    checkCheat(shortestPath, grid, invertGrid, onePos, twoPos, oneOrder, twoOrder, savedCounts, seenCuts);
-////                }
-            }
-        }
-        
-//        FuncMap.from(savedCounts).entries().filter(entry -> entry.getKey() >= 50).sortedBy(Map.Entry::getKey).forEach(println);
-//        
-//        return FuncMap.from(savedCounts).entries().filter(entry -> entry.getKey() >= 100).map(Map.Entry::getValue).sumToInt(AtomicInteger::intValue);
-        return count;
-    }
-    
-    private Grid invertGrid(Grid grid) {
-        var clone = grid.clone();
-        for (var r = 0; r < clone.height(); r++) {
-            for (var c = 0; c < clone.width(); c++) {
-                var ch = clone.data()[r][c].charAt(0);
-                clone.data()[r][c] = (ch == '.') ? "#" : ".";
-            }
-        }
-        return clone;
-    }
-
-    private void checkCheat(Tuple2<Long, FuncList<Position>> shortestPath, Grid orgGrid, Grid invertGrid, Position onePos, Position twoPos, int oneOrder, int twoOrder, HashMap<Long, AtomicInteger> savedCounts, HashMap<Long, Set<String>>seenCuts) {
-        var grid = new Grid(invertGrid.clone().data(), onePos, twoPos);
-        grid.data()[onePos.row()][onePos.col()] = ".";
-        grid.data()[twoPos.row()][twoPos.col()] = ".";
-        
-        var invGraph  = Graph.from(grid);
-        var invShPath = invGraph.shortestCostPath(false);
-        if (invShPath._1() > 20)
-            return;
-        if (invShPath._2().isEmpty())
-            return;
-        
-        var newGrid = orgGrid.clone();
-        invShPath._2().forEach(pos -> {
-            newGrid.data()[pos.row()][pos.col()] = ".";
-        });
-        var newGraph  = Graph.from(newGrid);
-        var newShPath = newGraph.shortestCostPath(false);
-        var newShDist = newShPath._1();
-        
-        var saved = abs(shortestPath._1() - newShDist);
-        if (saved >= 50) {
-//            println(grid);
-//            println(invShPath);
-//            println("newGrid: ");
-//            println(newGrid);
-            var cuts = new HashSet<Position>();
-            
-            var display = orgGrid.clone();
-            for (int r = 0; r < display.height(); r++) {
-                for (int c = 0; c < display.width(); c++) {
-                    var pos = new Position(r, c);
-                    if ((display.data()[r][c].equals("#")) && newShPath._2().contains(pos)) {
-                        cuts.add(pos);
-                        display.data()[r][c] = BOLD + YELLOW + "+" + RESET;
-                    } else if (shortestPath._2().contains(pos) && (!newShPath._2().contains(pos) || (display.data()[r][c].equals(".")))) {
-                        display.data()[r][c] = BOLD + BLUE + "+" + RESET;
-                    }
-                }
-            }
-            display.data()[display.end().row()][display.end().col()]     = BOLD + GREEN + "@" + RESET;
-            display.data()[display.start().row()][display.start().col()] = BOLD + RED + "@" + RESET;
-            
-            var enterExit = newShPath._2().mapWithIndex().filter(pair -> !cuts.contains(pair._2)).mapTwo().filter(ab -> (ab._2()._1 - ab._1()._1) > 1).flatMap(ab -> FuncList.of(ab._1()._2, ab._2()._2));
-            
-            seenCuts.putIfAbsent(saved, new HashSet<String>());
-            if ((enterExit.size() == 2) && !seenCuts.get(saved).contains(enterExit.toString())) {
-                println("onePos: " + onePos + ", twoPos: " + twoPos + ", saved: " + saved);
-//                println(display);
-//                println(enterExit);
-//                println();
-                
-                seenCuts.get(saved).add(enterExit.toString());
-                savedCounts.putIfAbsent(saved, new AtomicInteger());
-                savedCounts.get(saved).incrementAndGet();
-            }
-        }
-    }
     
     //== Test ==
     
     @Test
     public void testExample() {
         var lines  = readAllLines();
-        var result = calulate(lines);
+        var result = new Day20Part1Test().calulate(lines, 20, 100);
         println("result: " + result);
         assertAsString("0", result);
     }
@@ -160,7 +19,7 @@ public class Day20Part2Test extends BaseTest {
     @Test
     public void testProd() {
         var lines  = readAllLines();
-        var result = calulate(lines);
+        var result = new Day20Part1Test().calulate(lines, 20, 100);
         println("result: " + result);
         assertAsString("982474", result);
     }
