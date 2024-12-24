28453f64d433bf5bf55cd3b7e13479f629fe9261
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-24 04:03:48 -0500

    Save Day 24.

--- src/test/java/day24/Day24Part1Test.java
+++ src/test/java/day24/Day24Part1Test.java
@@ -2,15 +2,20 @@ package day24;
 
 import static functionalj.function.Func.f;
 import static functionalj.functions.StrFuncs.capture;
+import static java.util.regex.Pattern.compile;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
 import common.BaseTest;
 import functionalj.function.Func1;
 import functionalj.list.FuncList;
+import functionalj.map.FuncMap;
 import functionalj.types.Choice;
+import functionalj.types.choice.Self;
 
 public class Day24Part1Test extends BaseTest {
     
@@ -31,7 +36,14 @@ public class Day24Part1Test extends BaseTest {
     static interface NodeSpec {
         void Value(String name, int value);
         void Ref  (String name, Func1<String, Integer> context);
-        void Eval (String name, Operator operator, Node input1, Node input2);
+        void Eval (String name, Node input1, Node input2, Operator operator);
+        
+        public default String name(Self node) {
+            return ((Node)node.unwrap()).match()
+                    .value(Node.Value::name)
+                    .ref  (Node.Ref::name)
+                    .eval (Node.Eval::name);
+        }
     }
     
     static int nodeEval(Node node) {
@@ -41,47 +53,113 @@ public class Day24Part1Test extends BaseTest {
                 .eval (e -> e.operator().run(e.input1(), e.input2()));
     }
     
-    Object calulate(FuncList<String> lines) {
-        var nodes   = new ConcurrentHashMap<String, Node>();
-        var values  = new ConcurrentHashMap<String, Integer>();
-        var context = f("context", (Func1<String, Integer>)(nodeName -> {
+    static class Computer {
+        
+        private static final Pattern LOGIC = compile("(?<in1>[^ ]+) (?<opr>[^ ]+) (?<in2>[^ ]+) -> (?<out>[^ ]+)");
+        
+        final ConcurrentHashMap<String, Node>    nodes;
+        final ConcurrentHashMap<String, Integer> values;
+        
+        public Computer(FuncList<String> logicCode) {
+            this(logicCode, Map.of());
+        }
+        
+        public Computer(FuncList<String> logicCode, Map<String, Integer> inputs) {
+            this();
+            
+            var valueOf = f("this", this::valueOf);
+            
+            logicCode
+            .map    (capture(LOGIC))
+            .toMap  (pair -> pair.get("out"),
+                     pair -> Node.Eval(
+                                 pair.get("out"),
+                                 Node.Ref(pair.get("in1"), valueOf),
+                                 Node.Ref(pair.get("in2"), valueOf),
+                                 Operator.valueOf(pair.get("opr"))))
+            .forEach(nodes::put);
+            
+            inputs.forEach(values::put);
+            inputs.forEach((name, value) -> nodes.put(name, Node.Value(name, value)));
+        }
+        
+        private Computer() {
+            this((ConcurrentHashMap<String, Node>)null, (ConcurrentHashMap<String, Integer>) null);
+        }
+        
+        private Computer(ConcurrentHashMap<String, Node> nodes, ConcurrentHashMap<String, Integer> values) {
+            this.nodes  = (nodes  != null) ? nodes  : new ConcurrentHashMap<String, Node>();
+            this.values = (values != null) ? values : new ConcurrentHashMap<String, Integer>();
+        }
+        
+        public Computer withNewInputs(Map<String, Integer> inputs) {
+            // NOTE: I know. ...
+            //   This is very hacky ..
+            //     ... but sort of need a way to switch in a set of inputs with the same logic
+            //     without having to re-compile the logic
+            //     and not effect the existing
+            //     as well as not working with memoization.
+            var newComputer = new Computer(new ConcurrentHashMap<>(nodes), new ConcurrentHashMap<>());
+            
+            var newNodes = FuncMap.from(newComputer.nodes);
+            newComputer.nodes.clear();
+            newComputer.nodes.putAll(
+                newNodes
+                .mapValue(node -> {
+                    return node.match()
+                            .value(v -> (Node)v)
+                            .ref  (r -> (Node)r)
+                            .eval (e -> (Node)e.withInput1(Node.Ref(e.input1().asRef().get().name(), n -> newComputer.valueOf(n)))
+                                               .withInput2(Node.Ref(e.input2().asRef().get().name(), n -> newComputer.valueOf(n))));
+                }));
+            
+            inputs.forEach(newComputer.values::put);
+            inputs.forEach((name, value) -> newComputer.nodes.put(name, Node.Value(name, value)));
+            return newComputer;
+        }
+        
+        public int valueOf(String nodeName) {
             var value = values.get(nodeName);
             if (value != null)
                 return value.intValue();
             
             var node = nodes.get(nodeName);
+            if(node == null)
+                throw new NullPointerException("Node is null. nodeName=" + nodeName);
             value = nodeEval(node);
             values.put(nodeName, value);
             return value;
-        }));
+        }
         
+        public long calculate(Map<String, Integer> inputs) {
+            return withNewInputs(inputs)
+                    .calculate();
+        }
+        
+        public long calculate() {
+            return FuncList.from(nodes.keySet())
+                    .filter(theString.thatStartsWith("z"))
+                    .sorted()
+                    .mapToLong    ( z     -> valueOf(z))
+                    .mapWithIndex((i, z) -> z << i)
+                    .sum();
+        }
+        
+    }
+    
+    Object calulate(FuncList<String> lines) {
         var inputs 
                 = lines
                 .acceptWhile(line -> !line.isEmpty())
                 .map(capture(regex("(?<key>[xy0-9]+): (?<value>[0-9]+)")))
-                .toMap(pair -> pair.get("key"), pair -> Node.Value(pair.get("key"), parseInt(pair.get("value"))))
+                .toMap(pair -> pair.get("key"), pair -> parseInt(pair.get("value")))
                 ;
         
-        var exprs
-                = lines
-                .skipUntil(line -> line.isEmpty())
-                .skip     (1)    // Skip the empty line itself.
-                .map      (capture(regex("(?<input1>[^ ]+) (?<operator>(OR|XOR|AND)) (?<input2>[^ ]+) -> (?<output>[^ ]+)")))
-                .toMap    (pair -> pair.get("output"), pair -> Node.Eval(pair.get("output"), Operator.valueOf(pair.get("operator")), Node.Ref(pair.get("input1"), context), Node.Ref(pair.get("input2"), context)));
-        
-        inputs.forEach((name, node) -> nodes.put(name, node));
-        exprs.forEach((name, node) -> nodes.put(name, node));
-        
-        FuncList.from(nodes.keySet())
-        .sorted()
-        .forEach( z -> show(z + ": ", context.apply(z)));
-        
-        return FuncList.from(nodes.keySet())
-                .filter(theString.thatStartsWith("z"))
-                .sorted()
-                .mapToLong    ( z     -> show(z + ": ", context.apply(z)))
-                .mapWithIndex((i, z) -> z << i)
-                .sum();
+        var logicCode = lines
+                        .skipUntil(line -> line.isEmpty())
+                        .skip     (1);
+        var computer = new Computer(logicCode, inputs);
+        return computer.calculate();
     }
     
     //== Test ==
--- src/test/java/day24/Day24Part2Test.java
+++ src/test/java/day24/Day24Part2Test.java
@@ -1,36 +1,186 @@
 package day24;
 
-import static functionalj.functions.StrFuncs.*;
+import static day24.Node.theNode;
+import static day24.Node.Eval.theEval;
+import static functionalj.stream.intstream.IntStreamPlus.range;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 import common.BaseTest;
+import day24.Day24Part1Test.Computer;
+import day24.Day24Part1Test.Operator;
 import functionalj.list.FuncList;
+import functionalj.map.FuncMap;
+import functionalj.tuple.Tuple;
+import functionalj.tuple.Tuple3;
 
-@SuppressWarnings("unused")
-@Ignore
 public class Day24Part2Test extends BaseTest {
     
-    
     Object calulate(FuncList<String> lines) {
-        lines.forEach(this::println);
+        // Input of any expression is always right and output may be wrong.
+        
+        var logicCode
+                = lines
+                .skipUntil(line -> line.isEmpty())
+                .skip     (1)
+                .cache();
+        var computer = new Computer(logicCode);
+        
+        var maxDigit
+                = logicCode
+                .map     (grab(regex("-> z[0-9]+")))
+                .filter  (theList.thatIsNotEmpty())
+                .map     (theList.first().asString().replaceAll("[^0-9]", ""))
+                .mapToInt(parseInt)
+                .max()
+                .getAsInt();
+        
+        var inputs = new ConcurrentHashMap<String, Integer>();
+        range    (0, maxDigit + 1)
+        .mapToObj("%02d"::formatted)
+        .forEach (index -> {
+            inputs.put("x" + index, 0);
+            inputs.put("y" + index, 0);
+        });
+        println(computer.calculate(inputs));
         println();
         
-        return null;
+        // Full adder
+        //  A + B = Z
+        //  Zn = An XOR Bn XOR Cn_1
+        //  Cn = (An AND Bn) OR (Cn_1 AND (An_1 XOR Bn_1))
+        
+        // An XOR Bn ... is known as partial adder
+        
+        // So,
+        // Any XOR with an input is always for the same digit of the input.
+        // Any XOR that are not from input will be same digit with the input that comes from XOR and one after the one that come from OR
+        // Any AND are if the same digit
+        // Any OR are of the different digits.
+        // There is only one OR for output z ... that is the last carry.
+        
+        var aliases   = new ConcurrentHashMap<String, String>();
+        var expecteds = new ConcurrentHashMap<String, String>();
+        
+        var evals
+                = FuncList.from(computer.nodes.values())
+                .map        (theNode.asEval.get())
+                .excludeNull()
+                .map        (e -> Tuple.of(e.operator(), e.name(), FuncList.of(e.input1().name(), e.input2().name()).sorted()))
+                .sortedBy   (t -> t._1)
+                .cache();
+        evals.forEach(println);
+        println();
+        
+        println("Parial adders ");
+        var partialAdders
+            = evals
+            .filter(t -> t._1.equals(Operator.XOR))
+            .filter(t -> t._3.allMatch(s -> s.matches("^[xy][0-9]+$")))
+            .cache();
+        
+        partialAdders.forEach(t -> aliases.put(t._2, "p" + t._3.get(0).replaceAll("[^0-9]", "")));
+        partialAdders.forEach(println);
+        println();
+        
+        println("Aliases: ");
+        aliases.entrySet().forEach(println);
+        println();
+        
+        var invertAliases = FuncMap.from(aliases).entries().toMap(Map.Entry::getValue, Map.Entry::getKey);
+        
+        // TO-CHECK: z00 must be the same with p00
+        
+        // For everything but 00, pXX will be operated XOR with cYY where YY is one less than XX.
+        
+        var outputExprs
+                = evals
+                .filter(t -> t._1.equals(Operator.XOR))
+                .filter(t -> t._3.anyMatch(s -> aliases.containsKey(s)))
+                .map   (t -> t.map3(operands -> operands.filter(aliases::containsKey).map(aliases::get).get(0)))
+                .cache ();
+        
+        println("Ouptut exprs: ");
+        outputExprs.forEach(println);
+        println();
+        
+        var outputNodeWrongs
+                = outputExprs
+                .filter(t -> t._2().matches("^z[0-9]+$"))
+                .filter(t -> t._2().replaceAll("z", "").equals(t._3().replaceAll("p", "")))
+                .cache();
+        
+        println("Output Node Wrongs: ");
+        outputNodeWrongs.forEach(println);
+        println();
+        
+        var expectedOutputNodes
+                = outputExprs
+                .filter(t -> !t._2().matches("^z[0-9]+$"))
+                .toMap(t -> t._2(), t -> t._3().replaceAll("p", "z"));
+        
+        println("Expected Output Node: ");
+        expectedOutputNodes
+        .entries()
+        .forEach(println);
+        
+        
+//        
+//        // Output of XOR of partial adders must be z
+//        evals
+//        .filter(t -> t._1.equals(Operator.XOR))
+//        .filter(t -> partialAdderNames.contains(t._2))
+//        .filter(t -> aliases.get(t._2).equals(t))
+//        ;
+//        
+//        var inputExprs
+//                = FuncList.from(computer.nodes.values())
+//                .map        (theNode.asEval.get())
+//                .excludeNull()
+//                .filter     (theEval.input1.asRef.get().name.thatMatches("[xy][0-9]{2}"))
+//                .map        (e -> Tuple.of(e.operator(), e.input1().asRef().get().name().replaceAll("[xy]", ""), e.name()))
+//                .sortedBy   (String::valueOf)
+//                .cache();
+//        
+//        println("Input expressions: ");
+//        inputExprs
+//        .forEach(println);
+//        println();
+//        
+//        // We must check that 
+//        
+//        var outputExprs
+//                = FuncList.from(computer.nodes.values())
+//                .map        (theNode.asEval.get())
+//                .excludeNull()
+//                .filter     (theEval.name.thatMatches("z[0-9]{2}"))
+//                .map        (e -> Tuple.of(e.operator(), e.name(), e.input1().name(), e.input2().name()))
+//                .sortedBy   (String::valueOf)
+//                .cache();
+//        
+//        println("Output expressions: ");
+//        outputExprs
+//        .forEach(println);
+//        println();
+        
+        return computer.calculate(inputs);
     }
     
     //== Test ==
     
+    @Ignore
     @Test
     public void testExample() {
         var lines  = readAllLines();
         var result = calulate(lines);
         println("result: " + result);
-        assertAsString("", result);
+        assertAsString("z00,z01,z02,z05", result);
     }
     
-    @Ignore
     @Test
     public void testProd() {
         var lines  = readAllLines();
