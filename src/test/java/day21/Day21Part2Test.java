9662f483e578f2017b63ce3f2b6cd4de45ff71f9
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-28 17:42:52 -0500

    First of Day 21 part 2.

--- src/test/java/day21/Day21Part2Test.java
+++ src/test/java/day21/Day21Part2Test.java
@@ -1,40 +1,59 @@
 package day21;
 
-import static functionalj.functions.StrFuncs.*;
+import static functionalj.function.Func.f;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import common.BaseTest;
+import day21.Day21Part1Test.ArrowPad;
+import day21.Day21Part1Test.NumberPad;
+import functionalj.function.Func;
 import functionalj.list.FuncList;
+import functionalj.pipeable.Pipeable;
 
-@SuppressWarnings("unused")
-@Ignore
 public class Day21Part2Test extends BaseTest {
     
-    
-    Object calulate(FuncList<String> lines) {
-        lines.forEach(this::println);
-        println();
+    Object calculate(FuncList<String> lines) {
+        var doorPad    = new NumberPad();
+        int robotCount = 2;
+        var robotPads  = FuncList.generate(() -> f(ArrowPad::new)).limit(robotCount);
+        var calculate  = Func.f((String target) -> {
+            // Reset all pads
+            doorPad.reset();
+            robotPads.forEach(ArrowPad::reset);
+            
+            var shortest
+                    = Pipeable.of(target)
+                    .pipeTo(k -> doorPad.determineKeyPressed(k));
+            
+            for (ArrowPad robotPad : robotPads) {
+                shortest = shortest.flatMap(k -> robotPad.determineKeyPressed(k));
+            }
+            
+            var shortestPath = shortest.minBy(String::length);
+            var numberPart   = Long.parseLong(target.replaceAll("[^0-9]+", ""));
+            println(target + ": " + (long) shortestPath.get().length() + " -- " + numberPart);
+            return numberPart * (long) shortestPath.get().length();
+        });
         
-        return null;
+        return lines.sumToLong(calculate::apply);
     }
     
     //== Test ==
     
+//    @Ignore("Not asked.")
     @Test
     public void testExample() {
         var lines  = readAllLines();
-        var result = calulate(lines);
+        var result = calculate(lines);
         println("result: " + result);
         assertAsString("", result);
     }
     
-    @Ignore
     @Test
     public void testProd() {
         var lines  = readAllLines();
-        var result = calulate(lines);
+        var result = calculate(lines);
         println("result: " + result);
         assertAsString("", result);
     }
